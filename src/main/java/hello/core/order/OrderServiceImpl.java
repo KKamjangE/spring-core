18c311028bc916c4e3bdad39760c908effbd1c48
KKamjangE <ajm980718@gmail.com>
2023-09-26 14:40:46 +0900

    Feat: 주문 서비스 인터페이스와 구현체 구현

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,20 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{ // 주문 서비스 구현체
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository(); // 멤버 저장소
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // 할인 정책
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId); // 회원 찾기
+        int discountPrice = discountPolicy.discount(member, itemPrice);// 할인 가격
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
