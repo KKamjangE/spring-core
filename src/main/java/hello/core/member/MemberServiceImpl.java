e88bc32e66cff6192abdabf95ed4a38191719794
KKamjangE <ajm980718@gmail.com>
2023-09-26 16:05:05 +0900

    Refactor: 회원 서비스 의존성 주입으로 변경

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,8 +1,12 @@
 package hello.core.member;
 
-public class MemberServiceImpl implements MemberService{
+public class MemberServiceImpl implements MemberService{ // 회원 서비스 구현체
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) { // 생성자
+        this.memberRepository = memberRepository; // 의존성 주입
+    }
 
     @Override
     public void join(Member member) {
