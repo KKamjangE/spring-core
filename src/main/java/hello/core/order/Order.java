6b9d9124f95929e7e369e77de5b85940cb86664a
KKamjangE <ajm980718@gmail.com>
2023-09-26 14:40:36 +0900

    Feat: 주문 객체 구현

---
+++ src/main/java/hello/core/order/Order.java
@@ -0,0 +1,61 @@
+package hello.core.order;
+
+public class Order { // 주문 클래스
+    private Long memberId; // member 객체 id
+    private String itemName; // 상품 이름
+    private int itemPrice; // 상품 가격
+    private int discountPrice; // 할인 가격
+
+    public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
+        this.memberId = memberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.discountPrice = discountPrice;
+    }
+
+    public int calculatePrice() { // 할인 가격 계산
+        return itemPrice - discountPrice;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public void setDiscountPrice(int discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+
+    @Override
+    public String toString() { // 객체를 출력하면 이 문자열이 출력된다
+        return "Order{" +
+                "memberId=" + memberId +
+                ", itemName='" + itemName + '\'' +
+                ", itemPrice=" + itemPrice +
+                ", discountPrice=" + discountPrice +
+                '}';
+    }
+}
