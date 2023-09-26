fd458e170a90b3ffcd59e07e32d089d752ecb076
KKamjangE <ajm980718@gmail.com>
2023-09-26 15:16:29 +0900

    Refactor:  OrderServiceImpl이 discountPolicy의 구현체에 의존하지 않도록 변경

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +8,8 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{ // 주문 서비스 구현체
 
     private final MemberRepository memberRepository = new MemoryMemberRepository(); // 멤버 저장소
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // 할인 정책
+    private DiscountPolicy discountPolicy;
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId); // 회원 찾기
