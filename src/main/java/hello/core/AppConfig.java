ae89a8a58ebf62ca7af5dc18be3e732e37d94e68
KKamjangE <ajm980718@gmail.com>
2023-09-26 16:14:26 +0900

    Refactor: 중복되는 객체 생성 합치기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,8 @@
 package hello.core;
 
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,12 +11,20 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig { // 객체의 생성과 연결 담당
 
-    public MemberService memberService() { // memberService 의존성 주입
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 메모리 회원 저장소로 주입
+    private MemberRepository memberRepository() { // 회원 저장소
+        return new MemoryMemberRepository();
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    public DiscountPolicy discountPolicy() { // 할인 정책
+        return new RateDiscountPolicy();
+    }
+
+    public MemberService memberService() { // 회원 서비스
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public OrderService orderService() { // 주문 서비스
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
 }
