174d467572c38724498e04f8b9b6b0abb91d7268
KKamjangE <ajm980718@gmail.com>
2023-10-04 13:45:01 +0900

    Test: Component Scan Filter 테스트

---
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,36 @@
+package hello.core.scan.filter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void filterScan() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+        assertThat(beanA).isNotNull(); // beanA가 Null이 아닌지 확인한다
+
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("beanB", BeanB.class)); // beanB가 없다면 테스트 성공
+    }
+
+    @Configuration
+    @ComponentScan(
+            // type = FilterType.ANNOTATION: 어노테이션과 관련된 필터(기본값이기 때문에 생략)
+            includeFilters = @Filter(classes = MyIncludeComponent.class),
+            excludeFilters = @Filter(classes = MyExcludeComponent.class)
+    )
+    static class ComponentFilterAppConfig {
+
+    }
+}
