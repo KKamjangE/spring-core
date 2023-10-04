c8516641dc48bd5f4377f5bd3da87883cb210714
KKamjangE <ajm980718@gmail.com>
2023-10-04 14:44:34 +0900

    Fix: Component Scan 탐색 시작 지점 변경

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 // @Component가 붙은 클래스를 모두 찾아서 빈으로 등록한다
 @ComponentScan(
-        basePackages = "hello.core.member", // 탐색할 패키지의 시작 위치 지정
+        basePackages = "hello.core", // 탐색할 패키지의 시작 위치 지정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // Configuration 이라는 어노테이션이 붙은 클래스는 제외한다
 )
 public class AutoAppConfig {
